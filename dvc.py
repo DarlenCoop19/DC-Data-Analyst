0dad1cc79cf868a1060f08aba16b7d707491f55a
Darlene Cooper <53108678+DarlenCoop19@users.noreply.github.com>
2019-07-29 19:38:34 -0700

    Update and rename README.md to dvc.py

---
+++ dvc.py
@@ -0,0 +1,11 @@
+#valdez
+
+my_name = "Darlene"
+
+greeting = "Hello, world, I'm "
+
+def say_hello(name):
+    if name == "":
+        print("You can't introduce yourself if you don't add your name!")
+    else:
+        print(greeting + name)
